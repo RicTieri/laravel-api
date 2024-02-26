be8f21b7655bcd935740390ac306a66b3aba9733
RicTieri <tieri.riccardo@gmail.com>
2024-02-26 17:30:44 +0100

    added controller function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,48 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        // ? EAGER LOADING con il nome del metodo presente all'interno del model
+        $projects = Project::with('title', 'author', 'date')->paginate(20);
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $projects,
+            ]
+        );
+    }
+
+    public function show(Project $project)
+    {
+        return response()->json([
+            "success" => true,
+            "results" => $project
+        ]);
+    }
+
+    public function search(Request $request)
+    {
+        $data = $request->all();
+
+        if (isset($data['title'])) {
+            $keyword = $data['title'];
+            $projects = Project::where('title', 'LIKE', "%{$keyword}%")->get();
+        } elseif (is_null($data['title'])) {
+            $projects = Project::all();
+        } else {
+            abort(404);
+        }
+
+        return response()->json([
+            "success" => true,
+            "results" => $projects,
+            "matches" => count($projects)
+        ]);
+    }
 }
